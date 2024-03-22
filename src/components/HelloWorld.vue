80d4f0e281f872d129bba6264a50f371fc422849
mainkeys <dymainkeys@gmail.com>
2024-03-22 23:43:23 +0800

    commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,48 @@
+<!--
+ * @Author: mainkeys dymainkeys@gmail.com
+ * @Date: 2024-03-22 23:20:54
+ * @LastEditors: mainkeys dymainkeys@gmail.com
+ * @LastEditTime: 2024-03-22 23:34:57
+ * @FilePath: \mks-midfront\src\components\HelloWorld.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <button type="button" @click="count++">count is {{ count }}</button>
+    <p>
+      Edit
+      <code>components/HelloWorld.vue</code> to test HMR
+    </p>
+  </div>
+
+  <p>
+    Check out
+    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
+      >create-vue</a
+    >, the official Vue + Vite starter
+  </p>
+  <p>
+    Install
+    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
+    in your IDE for a better DX sdfgsdfg
+  </p>
+  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
